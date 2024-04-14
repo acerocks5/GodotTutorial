42818fcd9fb339552eeb1f70d23faccc566e6cc3
Daniel Wegrzyn <danny.wegrzyn@hotmail.com>
2024-04-13 23:15:13 -0400

    added some collisions and movement inputs

---
+++ scenes/player/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+func _process(_delta):
+	
+	#input
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * 500
+	move_and_slide()
+	
+	#laser shooting input
+	if Input.is_action_just_pressed("primary action"):
+		print("shoot laser")
+		
+	#throw grenade input
+	if Input.is_action_just_pressed("secondary action"):
+		print("throw grenade")
+	
