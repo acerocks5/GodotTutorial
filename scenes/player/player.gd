d4634debc0f2a26724ba558481cfd967aa7985dd
Daniel Wegrzyn <danny.wegrzyn@hotmail.com>
2024-04-16 01:14:52 -0400

    added grenade and laser projectiles

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal shoot_laser()
-signal throw_grenade()
+signal shoot_laser(pos)
+signal throw_grenade(pos)
 
 var can_laser: bool = true
 var can_grenade: bool = true
@@ -15,15 +15,20 @@ func _process(_delta):
 	
 	#laser shooting input
 	if Input.is_action_just_pressed("primary action") and can_laser:
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		can_laser = false
 		$Timer_laser.start()
-		shoot_laser.emit()
+		shoot_laser.emit(selected_laser.global_position)
 		
 	#throw grenade input
 	if Input.is_action_just_pressed("secondary action") and can_grenade:
+		var grenade_markers = $LaserStartPositions.get_children()
+		var selected_grenade = grenade_markers[randi() % grenade_markers.size()]
+		#alternative to get position from the middle point: var pos = $LaserStartPositions.getChildren()[0].global_position, in emit put pos
 		can_grenade = false
 		$Timer_grenade.start()
-		throw_grenade.emit()
+		throw_grenade.emit(selected_grenade.global_position)
 
 func _on_timer_laser_timeout():
 	can_laser = true
